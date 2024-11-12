adade25ce689ff79aa85a56b11754692cf41ae86
Conner Teevan <connerteevan@gmail.com>
2024-11-12 14:40:24 -0800

    test

--- components/layout.js
+++ components/layout.js
@@ -1,29 +1,28 @@
 import Head from 'next/head';
 import Link from 'next/link';
-
-export default function Layout({children, home}) {
+// Test
+export default function Layout({ children, home }) {
   return (
     <>
       <Head>
         <title>Next.js App Practice</title>
       </Head>
       <header>
-        <nav className='m-3'>
+        <nav className="m-3">
           <Link href="/">Food.com</Link>
         </nav>
       </header>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
       {!home && (
-          <Link href="/" className='btn btn-primary mt-3'>
-            ← Back to home
-          </Link>
-        )
-      }
+        <Link href="/" className="btn btn-primary mt-3">
+          ← Back to home
+        </Link>
+      )}
       <footer>
-        <p className='text-center m-3'><em>where foodies meet foods</em></p>
+        <p className="text-center m-3">
+          <em>where foodies meet foods</em>
+        </p>
       </footer>
     </>
-  )
+  );
 }
